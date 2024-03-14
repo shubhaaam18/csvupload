0f8fabc84569ae4b077e0c46d8d812287e78bf5b
shubhaaam18 <103482230+shubhaaam18@users.noreply.github.com>
2024-03-14 19:25:56 +0530

    Update csvRoutes.js

--- routes/csvRoutes.js
+++ routes/csvRoutes.js
@@ -1,14 +1,19 @@
-const express = require('express');
-const router = express.Router();
-const multer = require('multer');
+// Importing the required modules
+const express = require('express'); // Importing Express.js framework
+const router = express.Router(); // Creating a router instance from Express.js
+const multer = require('multer'); // Importing Multer for handling file uploads
 
-const homeController = require('../controllers/home_controller');
-const csvController = require('../controllers/csv_controller');
-const upload = multer({ dest: 'uploads/'})
+// Importing controller modules
+const homeController = require('../controllers/home_controller'); // Importing home controller
+const csvController = require('../controllers/csv_controller'); // Importing CSV controller
 
+// Configuring Multer for file uploads
+const upload = multer({ dest: 'uploads/' }); // Setting the destination directory for uploaded files
 
-router.get('/', homeController.home);
-router.get('/csv/:id', csvController.view);
-router.post('/upload', upload.single('csv') ,csvController.upload);
+// Routes definition
+router.get('/', homeController.home); // Route for the home page
+router.get('/csv/:id', csvController.view); // Route for viewing a CSV file
+router.post('/upload', upload.single('csv'), csvController.upload); // Route for uploading a CSV file
 
+// Exporting the router
 module.exports = router;
