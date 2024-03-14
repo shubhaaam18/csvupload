97cccd53a41254fc26e211ffac3ff83713f26fd5
shubhaaam18 <103482230+shubhaaam18@users.noreply.github.com>
2024-03-14 19:24:24 +0530

    Update csvModel.js

--- models/csvModel.js
+++ models/csvModel.js
@@ -1,18 +1,24 @@
+// Importing mongoose library
 const mongoose = require('mongoose');
 
+// Defining the schema for the CSV model
 const csvSchema = new mongoose.Schema({
+  // Defining schema for the filename field
   filename: {
-    type: String,
-    required: true,
+    type: String, // Filename is of type String
+    required: true, // Filename is required
   },
+  // Defining schema for the header_row field
   header_row: {
-    type: [Object],
+    type: [Object], // header_row is an array of objects
   },
+  // Defining schema for the data_rows field
   data_rows: {
-    type: [Object],
+    type: [Object], // data_rows is an array of objects
   },
 }, {
-  timestamps: true,
+  timestamps: true, // Adding timestamps for createdAt and updatedAt
 });
 
+// Creating and exporting the CSV model using the schema defined above
 module.exports = mongoose.model('CSV', csvSchema);
