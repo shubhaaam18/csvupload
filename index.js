cbd494298de053fe94f0aaf6829910438a492ade
shubham pardesi <shubhampardesi8@gmail.com>
2024-03-15 12:56:47 +0530

    Third Commit

--- index.js
+++ index.js
@@ -5,22 +5,23 @@ const path = require('path');
 const expressLayouts = require('express-ejs-layouts');
 const db = require('./config/mongoose');
 const dotenv = require('dotenv');
+const cors = require('cors'); // Import CORS middleware
+const bodyParser = require('body-parser'); // Import body parsing middleware
 
 dotenv.config({ path: 'config/.env' });
 app.use(expressLayouts);
+app.use(cors()); // CORS middleware
+app.use(bodyParser.urlencoded({ extended: true })); // Use body parsing middleware for URL-encoded data
 
-//ejs
+// EJS
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-
-
-//routes
+// Routes
 app.use('/', require('./routes/csvRoutes'));
 
-//server listening
+// Server listening
 app.listen(PORT, (err) => {
-    if(err) console.log("error listening on", PORT);
-
-    console.log('listening on port', PORT);
-})
+    if(err) console.log("Error listening on", PORT);
+    console.log('Listening on port', PORT);
+});
